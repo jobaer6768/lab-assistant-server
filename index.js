1b2bd170f9cf286b71eb4b4e4902fbda1a2ba904
jobaer6768 <jobaer9898@gmail.com>
2024-03-12 01:54:02 +0600

    another commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
-console.log(process.env.PORT);
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (_req, res) => {
   res.json({ message: "Home Route" });
