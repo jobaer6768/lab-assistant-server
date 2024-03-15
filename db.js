809c11379fc88bbf9d51abc8f4c180c6370c7a88
jobaer6768 <jobaer9898@gmail.com>
2024-03-15 23:43:19 +0600

    working with local database

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDB(connectionStr) {
-  return mongoose.connect(connectionStr, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  return mongoose.connect(connectionStr);
 }
 
 module.exports = connectDB;
